70a63145b72d5062df5f58bdc9c0e8e4e3d71150
Ankit Bansal <iotdevelope@gmail.com>
2024-12-17 17:51:14 +0530

    Added few examples

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -5,9 +5,18 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main,LOG_LEVEL_DBG);
 
 int main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	float f = 3.14;
+	LOG_INF("Hello World! %s\r\n", CONFIG_BOARD);
+	LOG_INF("float value is %0.2f\r\n", f);
+	LOG_ERR("This is an error message");
+	LOG_WRN("This is a warning message");
+	LOG_INF("This is an info message");
+	LOG_DBG("This is a debug message");
 	return 0;
 }
