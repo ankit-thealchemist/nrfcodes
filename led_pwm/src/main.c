2172482dd51df97394208ee0f9babf1d12fd6073
Ankit Bansal <iotdevelope@gmail.com>
2024-12-29 13:56:01 +0530

    Added the pwm with custom board

--- led_pwm/src/main.c
+++ led_pwm/src/main.c
@@ -5,10 +5,35 @@
  */
 
 #include <stdio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/pwm.h>
+
+#define PWM_LED0_NODE DT_ALIAS(pwm_led0)
+
+#define PWM_PERIOD_NS 200000000
+#define PWM_DUTY_CYCLE 14000000
+
+static struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(PWM_LED0_NODE);
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	int rc;
+	rc = pwm_is_ready_dt(&pwm_led0);
+	printf("Starting the application\r\n");
+	if(rc < 0)
+	{
+		printf("Error: PWM device %s is not ready\n", pwm_led0.dev->name);
+		return 1;
+	}
+
+	rc = pwm_set_dt(&pwm_led0, PWM_MSEC(40), PWM_MSEC(1));
+	if(rc < 0)
+	{
+		printf("Error: PWM device %s is not ready\n", pwm_led0.dev->name);
+		return 1;
+	}
+
+	printf("PWM device %s is ready\n", pwm_led0.dev->name);
 
 	return 0;
 }
